185243cb963d2ea38d14f867677c45a3208673b6
tage tapang <tagetage70@gmail.com>
2023-02-18 14:36:42 +0530

    adjusted display of month

--- Mainserver.js
+++ Mainserver.js
@@ -57,7 +57,16 @@ app.post("/salary",async(req,res)=>{
     try {
         let user = await salarypost.findOne({name: req.body.name,month: req.body.month});
         if(user){
-           res.render("salaryresult",{user: user});
+          let temp = user.month;
+          let convertostring = temp.toString();
+          let spliting = convertostring.split(" ",4);
+          const mymonth = spliting[0] +" "+ spliting[1] +" "+ spliting[2] +" "+ spliting[3];
+          const obj = {
+            name: user.name,
+            salary: user.salary,
+            month: mymonth
+          }
+           res.render("salaryresult",{user: obj});
         }
         else{
             alert("user doesn't exist");
@@ -65,6 +74,7 @@ app.post("/salary",async(req,res)=>{
         }
         
     } catch (error) {
+      console.log(error.message);
         
     }
 
